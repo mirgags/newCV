770ce661bcefb5fea586159447507474b1ee27e3
mirgags <markmiraglia@gmail.com>
2016-11-03 16:08:37 -0700

    changed trackduck btn name

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ $(function () {
   template = Handlebars.compile(source);
 
   $("#rightNav").html(template(navList));
-  $("#rightNav").append('<div id="track-duck"></div>');
+  $("#rightNav").append('<div id="feedbackBtn"></div>');
  //if($("#pageContainer .regContent").text().length < 4) {
   if(!$.trim($("#pageContainer .regContent").html()).length) {
     function popHome(data) {
